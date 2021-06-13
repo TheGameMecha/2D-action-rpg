5f0cdaaa2f5c2a5c01af121d2584e32124e26b79
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2021-06-13 12:36:58 -0400

    setting up repo

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// All player input code goes here
+/// </summary>
+[RequireComponent(typeof(SpriteCharacterController))]
+public class PlayerInputController : MonoBehaviour
+{
+    SpriteCharacterController controller;
+    Vector2 input;
+
+    private void Awake()
+    {
+        controller = GetComponent<SpriteCharacterController>();
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        controller.SetMovement(input);
+    }
+}
---
+++ Assets/Scripts/SpriteCharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCharacterController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    Vector2 movement;
+    Vector2 lastMovement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetMovement(Vector2 input)
+    {
+        movement.x = input.x;
+        movement.y = input.y;
+    }
+
+    void Update()
+    {
+        if (movement.sqrMagnitude > 0)
+        {
+            lastMovement = movement;
+        }
+
+        // Update Animations
+        animator.SetFloat("Horizontal", lastMovement.x); // Use lastMovement so we can keep the idle animations facing the  correct direction
+        animator.SetFloat("Vertical", lastMovement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
