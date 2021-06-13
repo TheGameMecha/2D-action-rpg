3eddfefab3d5dc1c435eef7652b317b9763e541c
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2021-06-13 13:29:27 -0400

    Update Timer.cs
    -Added more helper functions and comments

--- Assets/Scripts/Utility/Timer.cs
+++ Assets/Scripts/Utility/Timer.cs
@@ -9,18 +9,29 @@ public class Timer
 
     float timer = 0f;
 
+    /// <summary>
+    /// Construct a new Timer
+    /// </summary>
+    /// <param name="_duration">The amount of time before the timer is done</param>
     public Timer(float _duration)
     {
         duration = _duration;
         isActive = false;
     }
 
+    /// <summary>
+    /// Begin the timer from the beginning
+    /// </summary>
     public void StartTimer()
     {
         isActive = true;
         timer = 0f;
     }
 
+    /// <summary>
+    /// Call this from your Update function
+    /// </summary>
+    /// <param name="delta">Delta time being used</param>
     public void Tick(float delta)
     {
         if (isActive)
@@ -32,8 +43,37 @@ public class Timer
         }
     }
 
+    /// <summary>
+    /// Pauses the timer without resetting the current time
+    /// </summary>
+    public void PauseTimer()
+    {
+        isActive = false;
+    }
+
+    /// <summary>
+    /// Unpauses the timer, leaving off when it was last paused
+    /// </summary>
+    public void UnpauseTimer()
+    {
+        isActive = true;
+    }
+
+    /// <summary>
+    /// Get if this timer has reached its duration
+    /// </summary>
+    /// <returns>Whether or not the timer has finished</returns>
     public bool IsDone()
     {
         return !isActive;
+    }
+
+    /// <summary>
+    /// Get the current value of the timer
+    /// </summary>
+    /// <returns>Returns the current time of the timer</returns>
+    public float GetCurrentTime()
+    {
+        return timer;
     }
 }
