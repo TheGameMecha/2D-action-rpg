740062efb69154f4c0f120399441a331d6831d51
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2021-06-13 23:33:22 -0400

    Updated tile assets

--- Assets/Scripts/SpriteCharacterController.cs
+++ Assets/Scripts/SpriteCharacterController.cs
@@ -7,15 +7,18 @@ public class SpriteCharacterController : MonoBehaviour
     [Header("Character Properties")]
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float attackCooldown = 0.1f;
+    [SerializeField] [Tooltip("The layers that this character can attack")] LayerMask combatLayers;
+    [SerializeField] float attackRange = 1.0f;
 
     Rigidbody2D rb;
     Animator animator;
     Timer attackCdTimer;
 
-
     Vector2 m_movement;
     Vector2 m_lastMovement;
     bool m_isAttacking;
+    FacingDirection m_facingDirection = FacingDirection.SOUTH;
+    Vector2 m_attackDirection;
 
     private void Awake()
     {
@@ -27,8 +30,11 @@ public class SpriteCharacterController : MonoBehaviour
 
     public void SetMovement(Vector2 input)
     {
-        m_movement.x = input.x;
-        m_movement.y = input.y;
+        if (!m_isAttacking)
+        {
+            m_movement.x = input.x;
+            m_movement.y = input.y;
+        }
     }
 
     public void PerformAttack()
@@ -43,7 +49,69 @@ public class SpriteCharacterController : MonoBehaviour
 
     void HandleAttackDetection()
     {
+        m_attackDirection = Vector2.right;
 
+        switch (m_facingDirection)
+        {
+            case FacingDirection.NORTH:
+                m_attackDirection = Vector2.up;
+                break;
+            case FacingDirection.SOUTH:
+                m_attackDirection = -Vector2.up;
+                break;
+            case FacingDirection.EAST:
+                m_attackDirection = Vector2.right;
+                break;
+            case FacingDirection.WEST:
+                m_attackDirection = -Vector2.right;
+                break;
+            default:
+                break;
+        }
+
+        Collider2D[] hitTargets = Physics2D.OverlapCircleAll(rb.position + (m_attackDirection/2), attackRange, combatLayers.value);
+
+        for (int i = 0; i < hitTargets.Length; i++)
+        {
+            Debug.Log(hitTargets[i]);
+
+        }
+    }
+
+    void UpdateFacingDirection()
+    {
+        if (m_lastMovement.x > 0 && m_lastMovement.y > 0)
+        {
+            m_facingDirection = FacingDirection.NORTH;
+        }
+        else if (m_lastMovement.x < 0 && m_lastMovement.y < 0)
+        {
+            m_facingDirection = FacingDirection.SOUTH;
+        }
+        else if (m_lastMovement.x < 0 && m_lastMovement.y > 0)
+        {
+            m_facingDirection = FacingDirection.NORTH;
+        }
+        else if (m_lastMovement.x > 0 && m_lastMovement.y < 0)
+        {
+            m_facingDirection = FacingDirection.SOUTH;
+        }
+        else if (m_lastMovement.x > 0)
+        {
+            m_facingDirection = FacingDirection.EAST;
+        }
+        else if (m_lastMovement.x < 0)
+        {
+            m_facingDirection = FacingDirection.WEST;
+        }
+        else if (m_lastMovement.y > 0)
+        {
+            m_facingDirection = FacingDirection.NORTH;
+        }
+        else if (m_lastMovement.y < 0)
+        {
+            m_facingDirection = FacingDirection.SOUTH;
+        }
     }
 
     void Update()
@@ -64,10 +132,33 @@ public class SpriteCharacterController : MonoBehaviour
         {
             m_isAttacking = false;
         }
+
+        UpdateFacingDirection();
+
+        if (m_isAttacking)
+        {
+            HandleAttackDetection();
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + m_movement * movementSpeed * Time.fixedDeltaTime);
+        if (!m_isAttacking)
+            rb.MovePosition(rb.position + m_movement * movementSpeed * Time.fixedDeltaTime);
     }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying && m_isAttacking)
+            Gizmos.DrawWireSphere(rb.position + (m_attackDirection/2), attackRange);
+    }
+}
+
+[System.Serializable]
+public enum FacingDirection
+{
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
 }
