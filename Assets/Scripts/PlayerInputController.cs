b44bbc35042c860b4cc46f25984b50071fa437e1
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2021-06-17 11:39:56 -0400

    hooked up Unity's new InputSystem
    still more work to do

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// All player input code goes here
@@ -10,10 +11,15 @@ public class PlayerInputController : MonoBehaviour
 {
     SpriteCharacterController controller;
     Vector2 input;
+    PlayerControlScheme controls;
 
     private void Awake()
     {
         controller = GetComponent<SpriteCharacterController>();
+        controls = new PlayerControlScheme();
+
+        controls.Player.Move.performed += ctx => input = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => input = Vector2.zero;
     }
 
     void Update()
@@ -24,19 +30,30 @@ public class PlayerInputController : MonoBehaviour
             return;
         }
 
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
+
+        
+        //input.x = Input.GetAxisRaw("Horizontal");
+        //input.y = Input.GetAxisRaw("Vertical");
 
         controller.SetMovement(input);
 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            controller.PerformAttack();
-        }
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    controller.PerformAttack();
+        //}
 
-        if (Input.GetButtonDown("Fire2"))
-        {
-            controller.PerformInteraction();
-        }
+        //if (Input.GetButtonDown("Fire2"))
+        //{
+        //    controller.PerformInteraction();
+        //}
+    }
+
+    void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+    void OnDisable()
+    {
+        controls.Player.Disable();
     }
 }
